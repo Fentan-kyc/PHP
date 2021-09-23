a3184879ee4c4025c5c2f67380bb7c54ba37ae6c
Fentan-kyc <di-ma02@mail.ru>
2021-09-23 14:23:08 +0300

    finished header(no adp)

--- php/header.php
+++ php/header.php
@@ -1,5 +1,23 @@
 <header class="header">
     <div class="container header__container">
-        
+        <div class="header__inner">
+            <a href="/" class="logo header__logo">
+                <img src="img/logo.png" alt="logo">
+            </a>
+
+            <nav class="header__menu">
+                <ul class="header__list list">
+                    <li class="header__item"><a href="/" class="header__link link">Главная</a></li>
+                    <li class="header__item"><a href="/about" class="header__link link">О нас</a></li>
+                    <li class="header__item"><a href="/services" class="header__link link">Услуги</a></li>
+                    <li class="header__item"><a href="/contacts" class="header__link link">Контакты</a></li>
+                </ul>
+            </nav>
+
+            <div class="header__sign">
+                <a href="/sign" class="header__link_sign_enter header__link_sign header__link link">Кабинет</a>
+                <a href="" class="header__link_sign header__link link">Выход</a>
+            </div>
+        </div>
     </div>
 </header>
