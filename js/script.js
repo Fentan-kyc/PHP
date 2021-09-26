485fd1170ee3f8863aa97b0f7a8602af77936c8c
Fentan-kyc <di-ma02@mail.ru>
2021-09-26 18:59:35 +0300

    slider pre_end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    $('.slider').slick();
+    $('.slider').slick({
+        autoplay: true,
+        autoplayspeed: 2100,
+        draggable: false,
+        touchThreshold: 3,
+        touchMove: false,
+        variablewidths: true
+    });
 });
