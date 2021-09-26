5a8802278e57790194cd13b5ee992eba81cf9f38
Fentan-kyc <di-ma02@mail.ru>
2021-09-26 13:17:15 +0300

    add js slider

--- index.php
+++ index.php
@@ -14,12 +14,22 @@
         <?php include 'php/header.php';?>
 
         <main class="main">
+            <section class="top">
+                <h1 class="title top__title">Топ публикаций</h1>
 
+                <div class="slider">
+
+                </div>
+            </section>
         </main>
 
         <footer class="footer">
             
         </footer>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
